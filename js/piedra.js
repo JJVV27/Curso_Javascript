09ac5faabd554e1ea775d3b2c0355658aec3f7ff
JJVV27 <jonysj27>
2015-04-30 12:01:59 -0500

    Curso_Javascript
    Terminado

--- js/piedra.js
+++ js/piedra.js
@@ -1,65 +1,90 @@
+function aleatorio(minimo,maximo)
+{
+	var numero = Math.floor(Math.random() * (maximo - minimo + 1 ) + minimo );
+	return numero;
+	}
+
 var piedra = 0;
 var papel = 1;
 var tijera = 2;
-
+var opciones =["Piedra" ,"Papel" , "Tijera"];
 
 var opcionUsuario;
-var opcionMaquina = 1;
+var opcionMaquina = aleatorio(0,2);
 //var opcionMaquina = Math.random(0,2);
 
 opcionUsuario = prompt("Qué eliges? \n Piedra : 0 \n Papel : 1 \n Tijera : 2" , 0);
 
 //alert("Tu elegiste : " + opcionUsuario);
 
+alert("Elegiste: " + opciones[opcionUsuario]);
+alert("Javascript eligió " + opciones[opcionMaquina]);
 if (opcionUsuario == piedra)
-{
-	alert("Elegiste Piedra!");
-	}if (opcionMaquina == piedra)
-	{
+        {
+		
+		
+		
+		if(opcionMaquina ==  piedra)
+		{
 		alert("Empate!");
 		}
-	else if (opcionMaquina == papel)
-	{
-		alert("Perdiste!");
+		else if(opcionMaquina == papel)
+		{
+		alert("Perdiste! :(")
+		}else if (opcionMaquina == tijera)
+		{
+	    alert("Ganaste!")
 		}
-	else if (opcionMaquina == tijera)
-	{
+		
+        }
+		
+else if (opcionUsuario == papel)
+
+		{
+		
+		
+		
+  		if(opcionMaquina == piedra)
+		{
 		alert("Ganaste!");
 		}
 		
+		else if (opcionMaquina == papel)
+		{
+		alert ("Empate!");
+		}
 		
-else if(opcionUsuario == 1)
-    
-	
-	{
-	alert("Elegiste Papel!");
-	
-	}
-//if(opcionMaquina == 0)
-//	{
-//		alert("Ganaste!");
-//		}
-	//	else if (opcionMaquina == 1)
-	//	{
-//			alert ("Empate!");
-//			}
-//		else if (opcionMaquina == 2)
-//		{
-	//		alert("Perdiste!");
-	//		}
-else if (opcionUsuario == 2){
-	alert("Elegiste Tijera");
-	}
+		
+		else if (opcionMaquina == tijera)
+		{
+		alert("Perdiste!  :(");
+		}
+		}
+		
+else if (opcionUsuario == tijera)
 
-//if(opcionMaquina == 0)
-//	{
-//		0alert("Perdiste!");
-//		}
-//	else if (opcionMaquina == 1)
-//	{
-//		alert("Ganaste!");
-//		}
-//	else if (opcionMaquina == 2)
-//	{
-//		alert("Empate!");
-//		}
+		{
+		
+		
+		
+		
+		if(opcionMaquina == piedra)
+		{
+		alert("Perdiste!   :(");
+		}
+		
+		
+		else if (opcionMaquina == papel)
+		{
+		alert("Ganaste!");
+		}
+		
+		else if (opcionMaquina == tijera)
+		{
+		alert("Empate! ");
+		}
+		}
+else 
+    {
+	alert("Porque elejiste esa opción , presiona F5");
+	}
